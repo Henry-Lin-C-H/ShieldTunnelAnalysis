e7ada2cd73dd7b272b5196759787d930c359c1a8
Henry-Lin-C-H <37204689+Henry-Lin-C-H@users.noreply.github.com>
2019-08-21 15:00:22 +0800

    Condition: Variation of 0.33% Diameter

---
+++ Class-SAP/SAP_VariationofDiameter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SinoTunnel
+{
+    class SAP_VariationofDiameter
+    {
+        string sectionUID;
+        GetWebData p;
+
+        public SAP_VariationofDiameter(string sectionUID)
+        {
+            this.sectionUID = sectionUID;
+            p = new GetWebData(sectionUID);      
+        }
+
+        public double VariationCal()
+        {
+            double variationPercent = 0.33;
+            double diameterInter = p.segmentRadiusInter * 2;//公尺(m)
+            double targetVariation = diameterInter * variationPercent / 100;//公尺 * 比例 * 百分比 :單位為公尺
+            
+            return targetVariation;
+        }
+    }
+}
