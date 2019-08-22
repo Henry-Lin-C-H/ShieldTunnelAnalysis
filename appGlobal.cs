cb39de556912c40edd59f0101b83bd1592cccf09
Henry-Lin-C-H <37204689+Henry-Lin-C-H@users.noreply.github.com>
2019-08-22 08:33:51 +0800

    To store the section user chose

---
+++ appGlobal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinoTunnel
+{
+    public class appGlobal
+    {
+        public static string IdentityUser = "";
+        public static string UserID = "";
+        public static string UserName = "";
+        public static string UserEmail = "";
+        public static bool IsMembership = false;
+        public static string projectUID = "";
+        public static string projectTitle = "";
+        public static string sectionUID = "";
+    }
+}
