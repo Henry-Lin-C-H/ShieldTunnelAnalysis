e00a5449b45a78f972b60022a788b93be5573e53
Henry-Lin-C-H <37204689+Henry-Lin-C-H@users.noreply.github.com>
2019-08-22 08:34:38 +0800

    To show the sectionUID and its description

---
+++ ComboItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinoTunnel
+{
+    class ComboItem
+    {
+        public ComboItem(string uid, string text)
+        {
+            UID = uid;
+            Text = text;
+        }
+
+        public string UID
+        { get; set; }
+
+        public string Text
+        { get; set; }
+
+        public override string ToString()
+        { return Text; }
+    }
+}
