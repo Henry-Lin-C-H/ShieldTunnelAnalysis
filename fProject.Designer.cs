f18483209fa31f3d31c1a8a21e67bd1f8ffe1289
Henry-Lin-C-H <37204689+Henry-Lin-C-H@users.noreply.github.com>
2019-08-26 08:42:05 +0800

    Select different project

---
+++ fProject.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace SinoTunnel
+{
+    partial class fProject
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ProjectSelect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(775, 352);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(460, 387);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "關閉";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ProjectSelect
+            // 
+            this.ProjectSelect.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.ProjectSelect.Location = new System.Drawing.Point(261, 387);
+            this.ProjectSelect.Name = "ProjectSelect";
+            this.ProjectSelect.Size = new System.Drawing.Size(104, 41);
+            this.ProjectSelect.TabIndex = 2;
+            this.ProjectSelect.Text = "選擇";
+            this.ProjectSelect.UseVisualStyleBackColor = true;
+            this.ProjectSelect.Click += new System.EventHandler(this.ProjectSelect_Click);
+            // 
+            // fProject
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ProjectSelect);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "fProject";
+            this.Text = "fProject";
+            this.Load += new System.EventHandler(this.fProject_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ProjectSelect;
+    }
+}
---
+++ fProject.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+//using STN_SQL;
+
+namespace SinoTunnel
+{
+    public partial class fProject : Form
+    {
+       // STN_SQL.STN_data datasearch = new STN_SQL.STN_data();
+
+        ExcuteSQL dataSearch = new ExcuteSQL();
+
+        public Form fParent = null;
+
+        public fProject()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void fProject_Load(object sender, EventArgs e)
+        {
+            /*dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;*/
+            DataGridViewColumn col;
+
+            col = new DataGridViewTextBoxColumn();
+            col.Name = "ProjectID";
+            col.DataPropertyName = "ProjectID";
+            col.HeaderText = "標案編號";
+            col.ReadOnly = true;
+            col.Frozen = true;
+            dataGridView1.Columns.Add(col);
+
+            col = new DataGridViewTextBoxColumn();
+            col.Name = "ProjectName";
+            col.DataPropertyName = "ProjectName";            
+            col.HeaderText = "標案名稱";
+            col.ReadOnly = true;
+            dataGridView1.Columns.Add(col);
+
+            col = new DataGridViewTextBoxColumn();
+            col.Name = "Region";
+            col.DataPropertyName = "Region";
+            col.HeaderText = "地區";
+            col.ReadOnly = true;
+            dataGridView1.Columns.Add(col);
+
+            col = new DataGridViewTextBoxColumn();
+            col.Name = "SectionNum";
+            col.DataPropertyName = "SectionNum";
+            col.HeaderText = "斷面數量";
+            col.ReadOnly = true;
+            dataGridView1.Columns.Add(col);
+
+            col = new DataGridViewTextBoxColumn();
+            col.Name = "EmpName";
+            col.DataPropertyName = "EmpName";
+            col.HeaderText = "建立者";
+            col.ReadOnly = true;
+            dataGridView1.Columns.Add(col);
+
+            col = new DataGridViewTextBoxColumn();
+            col.Name = "CreateDate";
+            col.DataPropertyName = "CreateDate";
+            col.HeaderText = "建立日期";
+            col.ReadOnly = true;
+            dataGridView1.Columns.Add(col);
+
+            col = new DataGridViewTextBoxColumn();
+            col.Name = "UID";
+            col.DataPropertyName = "UID";
+            col.Visible = false;
+            dataGridView1.Columns.Add(col);
+
+            ListLoadData();
+        }
+
+        public void ListLoadData()
+        {
+            DataTable list = dataSearch.GetProject();
+
+            list.Columns.Remove("Description");
+            list.Columns.Remove("Stage");
+            list.Columns.Remove("IsDelete");
+            
+
+            list.Columns.Add("EmpName", typeof(string));
+            list.Columns.Add("SectionNum", typeof(int));
+
+            DataTable section = dataSearch.GetDataBySQL("SELECT * FROM STN_Section");
+            for(int i = 0; i < list.Rows.Count; i++)
+            {
+                string createUser = list.Rows[i]["CreateUser"].ToString();
+                DataTable dt = dataSearch.GetDataBySQL($"SELECT u.*,p.PropertyValuesString FROM aspnet_Users u LEFT OUTER JOIN aspnet_Profile p ON p.UserId=u.UserId WHERE (u.UserName='{createUser}')");
+                list.Rows[i]["EmpName"] = dt.Rows[0]["PropertyValuesString"].ToString();
+
+                int k = 0;
+                for(int j = 0; j < section.Rows.Count; j++)
+                {
+                    if (list.Rows[i]["UID"].ToString() == section.Rows[j]["Project"].ToString()) k++;
+                }
+                list.Rows[i]["SectionNum"] = k;
+            }
+            list.Columns.Remove("CreateUser");
+
+            dataGridView1.DataSource = list;
+        }
+
+        private void ProjectSelect_Click(object sender, EventArgs e)
+        {
+            if(dataGridView1.SelectedCells.Count == 0)
+            {
+                MessageBox.Show("請選擇標案");
+                return;
+            }
+
+            appGlobal.projectUID = dataGridView1.SelectedCells[0].OwningRow.Cells["UID"].Value.ToString();
+            appGlobal.projectTitle = $"{dataGridView1.SelectedCells[0].OwningRow.Cells["ProjectID"].Value} {dataGridView1.SelectedCells[0].OwningRow.Cells["ProjectName"].Value}";
+
+            if(fParent is Form1)
+            {
+                ((Form1)fParent).ShowSectionList();
+                //((Form1)fParent).UpdateData();
+            }
+
+            this.Close();
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            ProjectSelect_Click(null, null);
+        }
+    }
+}
